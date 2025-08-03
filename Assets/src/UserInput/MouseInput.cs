da92cfe627b911c65cad71b0e2e401d87cdc22c4
Nestor Batoon <batoonnestor@gmail.com>
2025-08-03 20:56:50 +1000

    basic user input stuff

---
+++ Assets/src/UserInput/MouseInput.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseInput : MonoBehaviour
+{
+    
+    private bool unitsSelected = false;
+    private bool hoveringOverEnemy = false;
+    private bool actionQueue = false;
+    
+    void Start()
+    {
+    }
+
+    void Update()
+    {
+
+    }
+
+}
